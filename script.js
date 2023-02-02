c80654b1e2ee19bbf89ca7e8c0542ab14604c188
Arsen4ik <arsenfazleev@gmail.com>
2023-02-02 19:56:53 +0300

    modernization

--- script.js
+++ script.js
@@ -36,17 +36,52 @@ let counter = 0;
 // console.log(table2)
 // html = ""
 function func2(){
-    let q = getNumber()
-
     counter++
 
-    td = `
-    <td style="background-color: ${getColor(q)};">${q}</td>
-    `
-    
-    table += `<td${td}</td>`
+    let q = getNumber();
+    let td1 = ``;
+    let td2 = ``;
+    let td3 = ``;
+    let td4 = ``;
+    let td5 = ``;
 
-    html = `<table><tr>${table}</tr></table>`
+    if(counter == 1){
+        td1 = `<td style="background-color: ${getColor(q)};">${q}</td>`
+        tr += td1
+        table += td1
+    } else if(counter == 2){
+        td2 = `<td style="background-color: ${getColor(q)};">${q}</td>`
+        tr += td2
+        table += td2
+    } else if(counter == 3){
+        td3 = `<td style="background-color: ${getColor(q)};">${q}</td>`
+        tr += td3
+        table += td3
+    } else if(counter == 4){
+        td4 = `<td style="background-color: ${getColor(q)};">${q}</td>`
+        tr += td4
+        table += td4
+    } else if (counter == 5) {
+        td5 = `<td style="background-color: ${getColor(q)};">${q}</td>`
+        tr += td5
+        table += td5
+        table += `<tr>${tr}</tr>`
+        counter = 0;
+        tr = ''
+    }
+    
+    // tr = `
+    // <td>${td1}</td>
+    // <td>${td2}</td>
+    // <td>${td3}</td>
+    // <td>${td4}</td>
+    // <td>${td5}</td>
+    // `
+
+
+    // table += `<tr>${tr}</tr>`
+
+    html = `<table>${table}</table>`
 
     document.body.innerHTML += html
 }
